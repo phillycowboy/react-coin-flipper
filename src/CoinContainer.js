e0022473aaf92e5c400b15adf0a94eec9d9f14fe
Ben Harter-Murphy <benhartermurphy@gmail.com>
2021-09-09 13:52:47 -0400

    added logic to coin flip

--- src/CoinContainer.js
+++ src/CoinContainer.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react'
+import { choice } from './helpers'
 
 class CoinContainer extends Component{
     static defaultProps = {
@@ -17,11 +18,35 @@ class CoinContainer extends Component{
         }
 
     }
+    flipCoin = () => {
+        const newCoin = choice(this.props.coins);
+        this.setState((prevState) => ({
+            currCoin: newCoin,
+            nFlips: prevState.nFlips + 1,
+            nHeads: prevState.nHeads + (newCoin.side === "heads" ? 1 : 0),
+            nTails: prevState.nTails + (newCoin.side === "tails" ? 1 : 0)
+            // let newState = {
+            //     ...prevState,
+
+            // }
+            // if(newCoin.side === "heads"){
+            //     newState.nHeads += 1;
+            // }else{
+            //     newState.nTails += 1;
+            // }
+            // return newState;
+        }))
+    }
+
+    handleClick = (e) => {
+        this.flipCoin();
+    }
 
     render(){
         return(
             <div className="CoinContainer">
                 <h2>Lets Flip a Coin!</h2>
+                <button onClick={this.handleClick}>Flip Me!</button>
                 <p>Out of {this.state.nFlips} flips, there have been {this.state.nHeads} heads, there have been {this.state.nTails} tails.</p>
             </div>
         )
