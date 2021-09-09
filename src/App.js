da8489bb04d442a5eabcd3badc8913e283d32593
Ben Harter-Murphy <benhartermurphy@gmail.com>
2021-09-09 13:29:28 -0400

    built coin container

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 // import logo from './logo.svg';
 import './App.css';
+import CoinContainer from './CoinContainer';
 
 function App() {
   return (
     <div className="App">
-      <h1>HEY THERE!</h1>
+      <CoinContainer />
     </div>
   );
 }
